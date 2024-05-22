6cd23b5c9cb074a7c1805b408ec4a7718f44a2e7
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-05-23 01:04:52 +0300

    metrics + reqs

---
+++ utils/metrics.py
@@ -0,0 +1,15 @@
+import sewar
+import numpy as np
+
+def Calculate_metrics_sewar(model,dataloader):
+ 
+    ssim_CG_sewar=[]
+    psnr_CG_sewar=[]
+    vif_CG_sewar=[]
+    for inp, tar in dataloader.take(-1):
+        prediction = model.gen_G(inp, training=False)
+        ssim_CG_sewar.append(sewar.full_ref.ssim(tar[0].numpy(), prediction[0].numpy(),MAX=2))
+        psnr_CG_sewar.append(sewar.full_ref.psnr(tar[0].numpy(), prediction[0].numpy(),MAX=2))
+        vif_CG_sewar.append(sewar.full_ref.vifp(tar[0].numpy(), prediction[0].numpy()))  
+
+    return ssim_CG_sewar,psnr_CG_sewar,vif_CG_sewar
