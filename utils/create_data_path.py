2e9a9ae10351e44b7659c2b12d01fbd1a9fa15f3
EHVB <ehabkamal2562@gmail.com>
2024-06-06 10:57:48 +0300

    create data path modified

--- utils/create_data_path.py
+++ utils/create_data_path.py
@@ -4,7 +4,21 @@ def create_data_path(input_dir,output_dir, extensions=['.png', '.jpg', '.jpeg'])
     def is_image_file(filename):
         return any(filename.lower().endswith(ext) for ext in extensions)
 
-    input_paths = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if is_image_file(f)]
-    output_paths = [os.path.join(output_dir, f) for f in os.listdir(output_dir) if is_image_file(f)]
+    output_files = [f for f in os.listdir(output_dir) if is_image_file(f)]
+    output_paths = [os.path.join(output_dir, f) for f in output_files]
+    input_paths = [os.path.join(input_dir, modify_filename(f)) for f in output_files]
 
-    return sorted(input_paths), sorted(output_paths)
+    return input_paths, output_paths
+
+def modify_filename(filename):
+    # Split the filename on underscores
+    parts = filename.split('_')
+    
+    # Modify the last part by replacing 'C' with 'MG'
+    parts[-1] = parts[-1].replace('C', 'MG')
+    
+    # Join the parts back together with underscores
+    new_filename = '_'.join(parts)
+    
+    return new_filename
+    
