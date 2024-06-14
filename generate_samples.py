22c15f52adf050202dab4b1fa52bb997c18f1509
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-14 21:09:01 +0300

    Update generate_samples.py

--- generate_samples.py
+++ generate_samples.py
@@ -9,7 +9,7 @@ from utils.generate_image import generate_images
 dm_dir = r"./CDD-CESM/Low energy images of CDD-CESM" # Replace with path of digital mammo directory
 cesm_dir = r"./CDD-CESM/Subtracted images of CDD-CESM" # Replace with path of contrast mammo directory
 
-dm_paths, cesm_paths = create_data_path(dm_dir, cesm_dir)
+dm_paths, cesm_paths = create_data_path_old(dm_dir, cesm_dir)
 
 
 
