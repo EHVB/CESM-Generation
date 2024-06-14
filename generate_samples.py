b99052f3be0107c8363dd632e29194194c592f81
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-14 22:40:00 +0300

    try not a function call

--- generate_samples.py
+++ generate_samples.py
@@ -4,6 +4,7 @@ from data.dataset import Dataset
 import keras
 from utils.create_data_path import create_data_path_old
 from utils.generate_image import generate_images
+import matplotlib.pyplot as plt
 
 
 dm_dir = r"./CDD-CESM/Low energy images of CDD-CESM" # Replace with path of digital mammo directory
@@ -23,5 +24,36 @@ cyclegan_model.built = True
 cyclegan_model.load_weights("./weights/cyclegan_pretrained.weights.h5") # path to new model weights 
 
 # generate 20 sample images 
+sample_num = 0
+number_of_samples = 20
+for test_input, target in train_ds:
+        if sample_num >= number_of_samples:
+            break
 
-generate_images(cyclegan_model.gen_G, train_ds, number_of_samples=20)
+        prediction = cyclegan_model.gen_G(test_input, training=False)
+        print(type(test_input))
+        print(type(target))
+        print(type(cyclegan_model.gen_G))
+        print(type(prediction))
+
+        plt.figure(figsize=(15, 15))
+        display_list = [test_input[0], target[0], prediction[0]]
+        title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+        for i in range(3):
+            plt.subplot(1, 3, i + 1)
+            plt.title(title[i])
+            # Getting the pixel values in the [0, 1] range to plot.
+            plt.imshow(display_list[i] * 0.5 + 0.5, cmap="gray")
+
+        if not os.path.exists('/sampleimages'):
+            os.makedirs('/sampleimages')
+            print(f"Directory '{'/sampleimages'}' created.")
+        else:
+            print(f"Directory '{'/sampleimages'}' already exists.")
+
+        # Save the figure with a filename based on the sample number
+        plt.savefig(f'/sampleimages/sample_plot_{sample_num}.png')
+        print(f"save sample {sample_num}")
+        plt.close()  # Close the figure to avoid memory issues
+        sample_num += 1
