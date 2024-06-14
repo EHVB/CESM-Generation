7a57b5ef7cd385ed4a15dd4cdd770c5b41589c1c
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-14 20:55:51 +0300

    fix error in generating samples

--- utils/generate_image.py
+++ utils/generate_image.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt 
+import os 
 
 def generate_images(model, dataloader, number_of_samples=5):
   
@@ -13,6 +14,13 @@ def generate_images(model, dataloader, number_of_samples=5):
         plt.title(title[i])
         # Getting the pixel values in the [0, 1] range to plot.
         plt.imshow(display_list[i]*0.5 +0.5 , cmap="gray")
+
+    if not os.path.exists('/sampleimages'):
+        os.makedirs('/sampleimages')
+        print(f"Directory '{'/sampleimages'}' created.")
+    else:
+        print(f"Directory '{'/sampleimages'}' already exists.")
+
 
     # Save the figure with a filename based on the sample number
     plt.savefig(f'/sampleimages/sample_plot_{sample_num}.png')
