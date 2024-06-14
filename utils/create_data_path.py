1a9a0e57d5de3a15fbef8efd8d633c04a77c74d6
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-14 20:28:55 +0300

    generate imgs script

--- utils/create_data_path.py
+++ utils/create_data_path.py
@@ -25,4 +25,16 @@ def modify_filename(filename):
     new_filename = '_'.join(parts)
     
     return new_filename
+
+
+def create_data_path_old(input_dir,output_dir, extensions=['.png', '.jpg', '.jpeg']):
+    
+    def is_image_file(filename):
+        return any(filename.lower().endswith(ext) for ext in extensions)
+
+    input_paths = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if is_image_file(f)]
+    output_paths = [os.path.join(output_dir, f) for f in os.listdir(output_dir) if is_image_file(f)]
+
+    return input_paths, output_paths
+    return sorted(input_paths), sorted(output_paths)
     
