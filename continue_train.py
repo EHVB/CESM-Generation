572eeaef3c45c1e40fc6adc4f3fdc3414fc9f817
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-15 00:19:42 +0300

    old

--- continue_train.py
+++ continue_train.py
@@ -1,4 +1,4 @@
-from utils.create_data_path import create_data_path
+from utils.create_data_path import create_data_path , create_data_path_old
 from models.cyclegan import get_cyclegan_model
 from data.dataset import Dataset
 import keras
@@ -6,7 +6,7 @@ import keras
 dm_dir = r"./CDD-CESM/Low energy images of CDD-CESM" # Replace with path of digital mammo directory
 cesm_dir = r"./CDD-CESM/Subtracted images of CDD-CESM" # Replace with path of contrast mammo directory
 
-dm_paths, cesm_paths = create_data_path(dm_dir, cesm_dir, "jpg")
+dm_paths, cesm_paths = create_data_path_old(dm_dir, cesm_dir, "jpg")
 
 print(dm_paths[123])
 print(cesm_paths[123])
