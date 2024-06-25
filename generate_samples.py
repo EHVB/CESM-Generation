6574c0a8d2737e4369e2fd6b6038e19c439a87a9
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-25 15:07:51 +0300

    try generate samples for old dataset

--- generate_samples.py
+++ generate_samples.py
@@ -1,19 +1,16 @@
-from utils.create_data_path import create_data_path
+from utils.create_data_path import create_data_path , create_data_path_old 
+from utils.generate_image import generate_images 
 from models.cyclegan import get_cyclegan_model
 from data.dataset import Dataset
 import keras
-from utils.create_data_path import create_data_path_old
-from utils.generate_image import generate_images
-import matplotlib.pyplot as plt 
-import os
-import tensorflow as tf
 
+dm_dir = r"/kaggle/input/pix2pix-output/train/trainA" # Replace with path of digital mammo directory
+cesm_dir = r"/kaggle/input/pix2pix-output/train/trainB" # Replace with path of contrast mammo directory
 
-dm_dir = r"./CDD-CESM/Low energy images of CDD-CESM" # Replace with path of digital mammo directory
-cesm_dir = r"./CDD-CESM/Subtracted images of CDD-CESM" # Replace with path of contrast mammo directory
+dm_paths, cesm_paths = create_data_path_old(dm_dir, cesm_dir)
 
-dm_paths, cesm_paths = create_data_path(dm_dir, cesm_dir)
-
+print(dm_paths[123])
+print(cesm_paths[123])
 
 
 DS = Dataset()
@@ -23,7 +20,7 @@ cyclegan_model = get_cyclegan_model()
 
 cyclegan_model.built = True
 
-cyclegan_model.load_weights("./weights/cyclegan_pretrained.weights.h5") # path to new model weights 
+cyclegan_model.load_weights("./weights/cyclegan_trained.weights.h5")
 
-# generate 20 sample images 
+
 generate_images(cyclegan_model.gen_G,train_ds,20)
