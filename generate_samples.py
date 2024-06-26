1b2c4b7db38798072e9bc20c6e5ba3c5dece64a8
Salmoon8 <93344447+Salmoon8@users.noreply.github.com>
2024-06-26 17:24:21 +0300

    scores

--- generate_samples.py
+++ generate_samples.py
@@ -1,5 +1,6 @@
 from utils.create_data_path import create_data_path , create_data_path_old 
 from utils.generate_image import generate_images 
+from utils.metrics import Calculate_metrics_sewar
 from models.cyclegan import get_cyclegan_model
 from data.dataset import Dataset
 import keras
@@ -21,3 +22,4 @@ cyclegan_model.load_weights("./cyclegan_tuned.weights.h5")
 
 
 generate_images(cyclegan_model.gen_G,test_ds,100)
+Calculate_metrics_sewar(cyclegan_model,test_ds)
--- utils/metrics.py
+++ utils/metrics.py
@@ -12,4 +12,6 @@ def Calculate_metrics_sewar(model,dataloader):
         psnr_CG_sewar.append(sewar.full_ref.psnr(tar[0].numpy(), prediction[0].numpy(),MAX=2))
         vif_CG_sewar.append(sewar.full_ref.vifp(tar[0].numpy(), prediction[0].numpy()))  
 
+    print("ssim:",np.mean(ssim_CG_sewar),"psnr:",np.mean(psnr_CG_sewar,"vif:",np.mean(vif_CG_sewar)))
+
     return ssim_CG_sewar,psnr_CG_sewar,vif_CG_sewar
